fb0a672e0591bd3e8774582f60ebf416d1678cff
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2023-07-29 07:48:51 +0300

    Написал функцию, которая позволяет пинговать удаленные хосты

---
+++ network_monitoring.py
@@ -0,0 +1,75 @@
+from ping3 import ping
+
+
+def __check_host(host: str) -> tuple:
+    """
+    Функция посылает 3 icmp пакета для проверки качества линии связи,
+    используя функцию ping. Функция ping возвращает: float - OK (среднее
+    время задержки), False - хост недоступен, None - ошибка в сети
+    :param host: ip адрес хоста
+    :return: Результат 3-х ping запросов в виде кортежа
+    """
+    result = []
+    for i in range(3):
+        result.append(ping(host))
+    return tuple(result)
+
+
+def handle_info_about_host(host_ip: str, host_name: str, queue = None) -> tuple:
+    """
+    Обрабатывает информация, полученную от check_host
+    execution_code = -1: произошла непредвиденная ошибка
+    execution_code = 0: запрашиваемый узел доступен, пингуется без ошибок
+    execution_code = 1: Присутствуют ошибки при пинге запрашиваемого узла
+    execution_code = 2: запрашиваемый узел недоступен
+    :param host_ip: ip адрес хоста
+    :param host_name: доменное имя хоста (узла связи)
+    :return:
+    """
+    try:
+        info = __check_host(host_ip)
+        if any(info) is None:
+            execution_code = -1
+            result = execution_code, host_ip, host_name
+            if queue:
+                queue.put(result)
+            return result
+        if all(info) is False:
+            execution_code = 2
+            result = execution_code, host_ip, host_name
+            if queue:
+                queue.put(result)
+            return result
+        successful_icmp_request = 0
+        total_delay = 0
+        for result in info:
+            if isinstance(result, float):
+                successful_icmp_request += 1
+                total_delay += result
+        average_delay = total_delay / successful_icmp_request
+        if successful_icmp_request == 3:
+            execution_code = 0
+            result = execution_code, host_ip, host_name, round(average_delay, 3)
+            if queue:
+                queue.put(result)
+            return result
+        else:
+            execution_code = 1
+            result = execution_code, host_ip, host_name,\
+                round(average_delay, 3), (3 - successful_icmp_request)
+            if queue:
+                queue.put(result)
+            return result
+    except OSError:
+        print('Возможно у Вас недостаточно прав для запуска данной команды')
+
+
+if __name__ == "__main__":
+    hosts = [('google.com', 'google.com'),
+             ('onliner.by', 'onliner.by'),
+             ('127.0.0.1', 'my_comp'),
+             ('172.19.5.110', 'NKU'),
+             ('172.19.5.112', 'NKU-2'),
+             ]
+    for host in hosts:
+        print(handle_info_about_host(host_ip=host[0], host_name=host[1]))
---
+++ temp_main.py
@@ -0,0 +1,59 @@
+from queue import Queue
+from threading import Thread
+import time
+import os
+import sys
+from network_monitoring import handle_info_about_host
+
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
+
+
+
+hosts = [('google.com', 'google.com'),
+         ('onliner.by', 'onliner.by'),
+         ('127.0.0.1', 'my_comp'),
+         ('172.19.5.110', 'AAA'),
+         ('10.10.15.101', 'BBB'),
+         ('13.17.22.12', 'CCC'),
+         ]
+
+
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f'Function {func.__name__} took {end_time - start_time} seconds to execute.')
+        return result
+    return wrapper
+
+
+@timer_decorator
+def check_available_all_hosts_with_threading():
+    queue = Queue()
+    threads = []
+    for host in hosts:
+        t = Thread(target=handle_info_about_host,
+                   args=(host[0], host[1], queue))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    results = []
+    while not queue.empty():
+        results.append(queue.get())
+    return results
+
+
+@timer_decorator
+def check_available_all_hosts_without_threading():
+    for host in hosts:
+        print(handle_info_about_host(host_ip=host[0], host_name=host[1]))
+
+
+if __name__ == "__main__":
+    print(check_available_all_hosts_with_threading())
+    check_available_all_hosts_without_threading()
